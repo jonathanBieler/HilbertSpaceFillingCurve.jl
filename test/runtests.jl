79ac085ba19e8fa83b76a0a493e7207ade537b4a
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2019-04-23 16:45:59 +0200

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,14 @@
-using HilbertSpaceFillingCurve, Compat
-using Compat.Test
+using HilbertSpaceFillingCurve
+using Test
+
+@testset "hilbert" begin
 
 d = 10
 for ndims in 2:3, nbits in [8,16]
     p = hilbert(d, ndims, nbits)
-    @assert d == hilbert(p, ndims, nbits)
+    @test d == hilbert(p, ndims, nbits)
 end
 
 @test_throws AssertionError hilbert(d, 2, 64)
+
+end
